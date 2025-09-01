10c0c0e5f75ac1d0198fdc6f550ee3c50c76af6e
IsabellePacheco <isapachecosenai@gmail.com>
2025-09-01 12:11:45 -0300

    Botão simples

---
+++ Flet-01-isabelle/2_botao_simples.py
@@ -0,0 +1,43 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Meu Primeiro Botao "
+    page.padding = 20
+
+    #criando um texto que sera modificado pelo botao 
+    mensagem = ft.Text(
+        value="Clique no botao abaixo!", 
+        size=20, 
+        text_align=ft.TextAlign.CENTER,
+        color=ft.Colors.BLUE
+    )
+
+    def botao_clicado(evento):
+        """
+        Sera executada quando o botao for clicado
+        o prametro 'evento' contem informacoes sobre o evento de clique
+        """
+
+        #mudando o texto da mensagem
+        mensagem.value = "Parabens voce clicou no botao !"
+        mensagem.color = ft.Colors.GREEN
+
+        #importante: sempre que modificamos elementos da interface, precisamos chamar page.update() para que as mudanças aparecam na tela 
+
+        page.update()
+
+    #criando um botao
+    meu_botao = ft.ElevatedButton(
+        text="Clique aqui!", #texto do botao
+        on_click=botao_clicado, #função que sera chamada quando o botao for clicado
+        width=200, #largura botao
+        height=50, #altura botao
+        bgcolor=ft.Colors.BLUE, #cor de fundo
+        color=ft.Colors.WHITE #cor do texto
+    )
+
+    #adicionando elementos a pagina
+    page.add(mensagem)
+    page.add(meu_botao)
+
+ft.app(target=main)
